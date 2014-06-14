60b359334eaa4a6db8731a86b90e316eec0b8d63
ffacon <francois.facon@gmail.com>
2014-06-14 15:54:13 +0200

    add ajax header for tapestry and inject ngResource

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -2,10 +2,13 @@
 
 /* App Module */
 
-angular.module('phonecat', []).
+angular.module('phonecat', ['ngRoute','ngResource']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/phones', {templateUrl: './Index:partial/phones',   controller: PhoneListCtrl}).
-      when('/phones/:phoneId', {templateUrl: './Index:partial/phone-details-step8', controller: PhoneDetailCtrl}).
+      when('/phones/:phoneId', {templateUrl: './Index:partial/phone-details-step8', controller: 'PhoneDetailCtrl'}).
       otherwise({redirectTo: '/phones'});
-}]);
+}]).config(['$httpProvider', function($httpProvider) {
+    //add header to allow tapestry to serve data
+    //see https://github.com/angular/angular.js/issues/1004
+    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';}]);
