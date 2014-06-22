9a9aa0e3f15cfe41d0cdf57bb2dfdf5ad3f22cd1
ffacon <francois.facon@gmail.com>
2014-06-22 22:53:58 +0200

    login

---
+++ src/main/java/dev/openshift/tapestry/angular/data/user/Login.java
@@ -0,0 +1,35 @@
+package dev.openshift.tapestry.angular.data.user;
+
+public class Login {
+
+    private String username;
+    private String password;
+    private String rememberMe;
+
+    public Login() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRememberMe() {
+        return rememberMe;
+    }
+
+    public void setRememberMe(String rememberMe) {
+        this.rememberMe = rememberMe;
+    }
+}
