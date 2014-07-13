e2b2f458d027a588052a8389e7293120860fc2f8
ffacon <francois.facon@gmail.com>
2014-07-13 11:25:33 +0200

    add RegisterController

--- src/main/webapp/js/controllers/controllers-index.js
+++ src/main/webapp/js/controllers/controllers-index.js
@@ -85,3 +85,36 @@ phonecat.controller('LogoutController', ['$location', 'AuthenticationSharedServi
     function ($location, AuthenticationSharedService) {
         AuthenticationSharedService.logout();
     }]);
+
+phonecat.controller('RegisterController', ['$scope', '$translate', 'Register',
+    function ($scope, $translate, Register) {
+        $scope.success = null;
+        $scope.error = null;
+        $scope.doNotMatch = null;
+        $scope.errorUserExists = null;
+        $scope.register = function () {
+            if ($scope.registerAccount.password != $scope.confirmPassword) {
+                $scope.doNotMatch = "ERROR";
+            } else {
+                $scope.registerAccount.langKey = $translate.use();
+                $scope.doNotMatch = null;
+                Register.save($scope.registerAccount,
+                    function (value, responseHeaders) {
+                        $scope.error = null;
+                        $scope.errorUserExists = null;
+                        $scope.success = 'OK';
+                    },
+                    function (httpResponse) {
+                        $scope.success = null;
+                        if (httpResponse.status === 304 &&
+                            httpResponse.data.error && httpResponse.data.error === "Not Modified") {
+                            $scope.error = null;
+                            $scope.errorUserExists = "ERROR";
+                        } else {
+                            $scope.error = "ERROR";
+                            $scope.errorUserExists = null;
+                        }
+                    });
+            }
+        }
+    }]);
