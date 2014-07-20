a36a54e68f71452dc235a1b2c56fdd0d8eb6a6af
ffacon <francois.facon@gmail.com>
2014-07-20 09:59:18 +0200

    UserDao Impl

---
+++ src/main/java/dev/openshift/tapestry/angular/services/UserDAOImpl.java
@@ -0,0 +1,34 @@
+package dev.openshift.tapestry.angular.services;
+
+import dev.openshift.tapestry.angular.data.user.User;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class UserDAOImpl implements UserDAO
+{
+    private final Session session;
+
+    public UserDAOImpl(Session session)
+    {
+        this.session = session;
+    }
+
+    public User getUserByLogin(String login){
+        return (User)session.createCriteria(User.class).add(Restrictions.eq("login", login)).uniqueResult();
+    }
+
+    public void add(User user)
+    {
+        user.setUri("/user-management/users/"+user.getLogin());
+        user.setLastModified(new Date());
+        List<String> roles = new ArrayList<String>();
+        roles.add("USER");
+        user.setRoles(roles);
+        session.save(user);
+    }
+
+}
