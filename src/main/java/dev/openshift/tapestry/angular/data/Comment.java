8d1c5cbf499d623ab76cd3a3ae6c6c494e330dd6
ffacon <francois.facon@gmail.com>
2014-07-21 21:23:33 +0200

    Add Comment DAO

--- src/main/java/dev/openshift/tapestry/angular/data/Comment.java
+++ src/main/java/dev/openshift/tapestry/angular/data/Comment.java
@@ -1,12 +1,19 @@
 package dev.openshift.tapestry.angular.data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Comment {
 
     private String phoneId;
     private String author;
     private String content;
     private int likes;
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int id;
 
     public Comment() {
