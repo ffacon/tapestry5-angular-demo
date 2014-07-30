5b4224f1b52b8afe9b5eda2e703734bcbfe30e00
ffacon <francois.facon@gmail.com>
2014-07-30 19:57:24 +0200

    update UserDatabase implementation

--- src/main/java/dev/openshift/tapestry/angular/services/UserDatabaseImpl.java
+++ src/main/java/dev/openshift/tapestry/angular/services/UserDatabaseImpl.java
@@ -1,6 +1,6 @@
 package dev.openshift.tapestry.angular.services;
 
-import dev.openshift.tapestry.angular.data.user.User;
+import dev.openshift.tapestry.angular.entity.User;
 
 import java.util.ArrayList;
 import java.util.Date;
