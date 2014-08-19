943e73e197779d1827ea59fbe4bf4de9b0ed9eca
ffacon <francois.facon@gmail.com>
2014-08-19 22:17:57 +0200

    update CommentService

--- src/main/webapp/js/services/CommentService.js
+++ src/main/webapp/js/services/CommentService.js
@@ -1,7 +1,7 @@
 
     angular.module('phonecat')
         .factory('CommentService',['$resource', function ($resource) {
-            return $resource(window.location.origin + window.location.pathname + 'api/json/phone/comments/:op/:id',{id:'@id'},{
+            return $resource(baseUrl + 'api/json/phone/comments/:op/:id',{id:'@id'},{
                 query: { method: 'GET',params:{id:'@id'}, isArray: true },
                 like: {method:'GET',params: {id:'@id',op:'like'}},
                 delete: {method:'DELETE',params: {id:'@id'}}
