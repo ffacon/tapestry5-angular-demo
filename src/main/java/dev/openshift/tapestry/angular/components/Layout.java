94eba6d9bbe8630074caa5d715e80b969dd84dd1
ffacon <francois.facon@gmail.com>
2014-08-21 23:30:43 +0200

    Rename Layout

--- src/main/java/dev/openshift/tapestry/angular/components/Layout.java
+++ src/main/java/dev/openshift/tapestry/angular/components/Layout.java
@@ -15,21 +15,6 @@
 //
 
 package dev.openshift.tapestry.angular.components;
-//
-//Copyright 2010 GOT5 (GO Tapestry 5)
-//
-//Licensed under the Apache License, Version 2.0 (the "License");
-//you may not use this file except in compliance with the License.
-//You may obtain a copy of the License at
-//
-//	http://www.apache.org/licenses/LICENSE-2.0
-//
-//Unless required by applicable law or agreed to in writing, software
-//distributed under the License is distributed on an "AS IS" BASIS,
-//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//See the License for the specific language governing permissions and
-//limitations under the License.
-//
 
 import org.apache.tapestry5.BindingConstants;
 import org.apache.tapestry5.ComponentResources;
@@ -55,14 +40,6 @@ public class Layout
 	@Inject
 	private Request request;
 	
-	/*
-	 * If true, then the AngularJavaScriptStack will be loaded each time
-	 * If false (default), the stack won't be loaded if the request is an AJAX request and the page 
-	 * is located in the pages.jquerymobilepages subpackage
-	 */
-	@Parameter(value="false")
-	private boolean forceJsStackLoad;
-	
 	@Inject
 	private JavaScriptSupport support;
 
@@ -81,16 +58,6 @@ public class Layout
     	writer.getDocument().raw("<!DOCTYPE html>");
         this.pageName = resources.getPageName();
     }
-    
-    /*@AfterRender
-    public void afterRender(){
-		if(forceJsStackLoad)
-			support.importStack(AngularJavaScriptStack.STACK_ID);
-		else 
-			if(!jQMobilePageIdentifier.isJQMobilePage())
-				support.importStack(AngularJavaScriptStack.STACK_ID);
-				
-    	//ATTENTION A L'ANNOTATION IMPORT !!!
-    }*/
+
 
 }
