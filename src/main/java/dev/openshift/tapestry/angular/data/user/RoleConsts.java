c5289b56b99a61f40e2a6aaf8c6f5e4c2d28cd73
ffacon <francois.facon@gmail.com>
2014-09-25 21:30:33 +0200

    add roleConsts

---
+++ src/main/java/dev/openshift/tapestry/angular/data/user/RoleConsts.java
@@ -0,0 +1,10 @@
+package dev.openshift.tapestry.angular.data.user;
+
+
+public final class RoleConsts {
+
+    public static final String ALL_ROLE = "*";
+    public static final String USER_ROLE = "USER";
+    public static final String ADMIN_ROLE = "ADMIN";
+
+}
