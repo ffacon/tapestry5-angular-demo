4e96d1f626dbc5b1e9a9c86de06d236c06a6023f
ffacon <francois.facon@gmail.com>
2014-09-29 20:23:49 +0200

    update security interceptor

--- src/main/java/dev/openshift/tapestry/angular/ws/security/SecurityInterceptor.java
+++ src/main/java/dev/openshift/tapestry/angular/ws/security/SecurityInterceptor.java
@@ -2,26 +2,27 @@ package dev.openshift.tapestry.angular.ws.security;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 import javax.annotation.security.DenyAll;
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
 import dev.openshift.tapestry.angular.entity.User;
 import dev.openshift.tapestry.angular.services.UserDAO;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.jboss.resteasy.core.Headers;
 import org.jboss.resteasy.core.ResourceMethodInvoker;
 import org.jboss.resteasy.core.ServerResponse;
 import org.jboss.resteasy.util.Base64;
+import org.tynamo.security.services.SecurityService;
 
 /**
  * This interceptor verify the access permissions for a user 
@@ -37,27 +38,7 @@ public class SecurityInterceptor implements javax.ws.rs.container.ContainerReque
 	private static final ServerResponse SERVER_ERROR = new ServerResponse("INTERNAL SERVER ERROR", 500, new Headers<Object>());;
 
     @Inject
-    UserDAO userDatabase;
-
-	private boolean isUserAllowed(final String username, final String password,	final Set<String> rolesSet) 
-	{
-		boolean isAllowed = false;
-		
-		//Step 1. Fetch password from database and match with password in argument
-        //If both match then get the defined role for user from database and continue; else return isAllowed [false]
-        User user = userDatabase.getUserByLogin(username);
-        if(user != null && user.getPassword().equals(password)){
-            List<String> userRoles = user.getRoles();
-
-            for(String s : userRoles)
-            if(rolesSet.contains(s))
-            {
-                isAllowed = true;
-            }
-        }
-
-		return isAllowed;
-	}
+    private SecurityService securityService;
 
 
     public void filter(ContainerRequestContext requestContext) throws IOException   {
@@ -89,32 +70,30 @@ public class SecurityInterceptor implements javax.ws.rs.container.ContainerReque
                 return;
             }
 
-            //Get encoded username and password
-            final String encodedUserPassword = authorization.get(0).replaceFirst(AUTHENTICATION_SCHEME + " ", "");
 
-            //Decode username and password
-            String usernameAndPassword;
-            try {
-                usernameAndPassword = new String(Base64.decode(encodedUserPassword));
-            } catch (IOException e) {
+            Subject subject = securityService.getSubject();
+            if (!subject.isAuthenticated()) {
                 requestContext.abortWith(SERVER_ERROR);
                 return;
-            }
-
-            //Split username and password tokens
-            final StringTokenizer tokenizer = new StringTokenizer(usernameAndPassword, ":");
-            final String username = tokenizer.nextToken();
-            final String password = tokenizer.nextToken();
+             }
 
 
             //Verify user access
             if(method.isAnnotationPresent(RolesAllowed.class))
             {
+                boolean isAllowed = false;
+
                 RolesAllowed rolesAnnotation = method.getAnnotation(RolesAllowed.class);
                 Set<String> rolesSet = new HashSet<String>(Arrays.asList(rolesAnnotation.value()));
 
-                //Is user valid?
-                if( ! isUserAllowed(username, password, rolesSet))
+                //Is user allowed?
+                for(String s : rolesSet)
+                    if(subject.hasRole(s))
+                    {
+                        isAllowed = true;
+                    }
+
+                if(!isAllowed)
                 {
                     requestContext.abortWith(ACCESS_DENIED);
                 }
